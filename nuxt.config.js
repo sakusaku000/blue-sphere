bf8b016d794335313c84abacbf6720c71760c0ad
sakusaku000 <harry.spncr@gmail.com>
2022-06-07 17:53:38 +0100

    added page title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blue-sphere',
+    title: '95 Degrees - Discord Community',
     htmlAttrs: {
       lang: 'en'
     },
