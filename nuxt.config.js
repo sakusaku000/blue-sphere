72cf28cf4ff6973ea121ed3796f79011cf0218fa
sakusaku000 <harry.spncr@gmail.com>
2022-06-08 09:57:20 +0100

    changed build target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,34 +16,14 @@ export default {
       { rel:"stylesheet", href:"https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@700;800&display=swap"}
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
     dirs: [
       '~/components',
       '~/components/base'
     ]
   },
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  target: 'static'
 }
