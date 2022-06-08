50c53b7e164808e75db0dc82240cf78d8fcfb5bf
sakusaku000 <harry.spncr@gmail.com>
2022-06-08 16:01:38 +0100

    adjusted anim duration speed for svg logo

--- components/base/NavBar/Logo.vue
+++ components/base/NavBar/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <a href="#landing">
         <!-- <img src="~/assets/logo/nav-logo.png" alt="95 Degrees" class="hover:opacity-75 duration-200" draggable="false"> -->
-        <NinetyFive class="hover:text-brand-light w-12 duration-200"/>
+        <NinetyFive class="hover:text-brand-light w-12 duration-100"/>
     </a>
 </template>
