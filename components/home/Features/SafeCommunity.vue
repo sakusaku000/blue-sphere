58d0d02846961b5427ce17465d419a32dd79c9a2
sakusaku000 <harry.spncr@gmail.com>
2022-06-08 12:17:24 +0100

    fixed safe and moderated text

--- components/home/Features/SafeCommunity.vue
+++ components/home/Features/SafeCommunity.vue
@@ -2,8 +2,8 @@
     <Card>
         <IconsLock class="w-16 mb-1 text-brand-dark"/>
         <h1 class="title text-3xl mt-4 mb-4">safe and moderated</h1>
-        <p>
-            several custom-coded bots run in our server which offer convenient and exclusive features, such as guardian and 95 degrees radio
+        <p class="mb-6">
+            our staff do their utmost to keep the community safe from unwanted and suspicious behaviour
         </p>
     </Card>
 </template>
