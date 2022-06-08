2cb5a2e03c9c703bd5d00624b08da5b06322ba6f
sakusaku000 <harry.spncr@gmail.com>
2022-06-08 09:34:13 +0100

    added border to main button

--- components/base/MainButton.vue
+++ components/base/MainButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-3 px-6 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-50 duration-100 text-xl cursor-pointer flex items-center">
+    <div class="p-3 px-6 bg-gray-200 text-gray-800 border border-gray-300 rounded-lg hover:bg-gray-50 duration-100 text-xl cursor-pointer flex items-center">
         <slot/>
     </div>
 </template>
