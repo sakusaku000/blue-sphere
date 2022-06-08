eac0993bf17cdbbd14c21964018a0558792289b4
sakusaku000 <harry.spncr@gmail.com>
2022-06-08 22:43:44 +0100

    updated meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,24 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name:"title", content:"95 Degrees - Discord Community"},
+      { name:"description", content:"Serving coffee and conversation 24/7 since 2016 - 95 Degrees is a welcoming Discord server with many exclusive features."},
+      { name:"keywords", content:"discord, server, community, 95 degrees, cafe"},
+      { name:"robots", content:"index, follow"},
+      { name:"language", content:"english"},
+      // opengraph
+      { property:"og:type", content:"website"},
+      { property:"og:url", content:"https://95degrees.cafe/"},
+      { property:"og:title", content:"95 Degrees Cafe"},
+      { property:"og:description", content:"Serving coffee and conversation 24/7 since 2016 - 95 Degrees is a welcoming Discord server with many exclusive features."},
+      { property:"og:image", content:"https://95degrees.cafe/nf-card.png"},
+      // twitter
+      { property:"twitter:card", content:"summary_large_image"},
+      { property:"twitter:url", content:"https://95degrees.cafe/"},
+      { property:"twitter:title", content:"95 Degrees Cafe"},
+      { property:"twitter:description", content:"Serving coffee and conversation 24/7 since 2016 - 95 Degrees is a welcoming Discord server with many exclusive features."},
+      { property:"twitter:image", content:"https://95degrees.cafe/nf-card.png"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
