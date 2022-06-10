4597a4f224b382ccf5965feccb5721a93b61b9c6
sakusaku000 <harry.spncr@gmail.com>
2022-06-10 15:07:59 +0100

    centered landing on all screen sizes

--- components/home/Landing/index.vue
+++ components/home/Landing/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="landing" class=" text-white h-screen shadow-lg p-4 md:p-0">
-        <div class="h-full flex flex-col items-center justify-center md:justify-end select-none z-50">
+        <div class="h-full flex flex-col items-center justify-center select-none z-50">
             <HomeLandingBackground/>
             <HomeLandingLogo/>
             <HomeLandingJoinSocials/>
