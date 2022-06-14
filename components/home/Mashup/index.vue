6bb5ed1277e02426b076535e705a551257c061db
sakusaku000 <harry.spncr@gmail.com>
2022-06-15 00:23:46 +0100

    added video background to mashup section

--- components/home/Mashup/index.vue
+++ components/home/Mashup/index.vue
@@ -1,19 +1,8 @@
 <template>
-    <Container id="mashup">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-10 text-center md:text-left items-center">
-            <div>
-                <img class="mb-8 md:w-5/6" src="~/assets/logo/mashup2022.png" alt="95 Degrees Mashup 2022" draggable="false">
-                <h2 class="text-xl font-bold font-title">our most ambitious show ever!</h2>
-                <p>
-                    the 95 degrees mashup is a yearly celebration of our community, with awards, community highlights, announcements, 
-                    giveaways and so much more in one live, interactive stream! this year we celebrated six years of our community, 
-                    and if you haven't seen mashup 2022 yet, you can <span class="inline md:hidden">watch it below!</span> <span class="hidden md:inline">watch it over on the right! >></span>
-                </p>
-            </div>
-            <div>
-                <iframe class="rounded-xl w-full h-96 shadow-xl" width="854" height="480" src="https://www.youtube.com/embed/ZkFEFrVl8hE?start=1978" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <span class="text-xs text-gray-400">(this is the 2021 mashup for now until 2022 is finished)</span>
-            </div>
-        </div>
-    </Container>
+    <div class="relative">
+        <!-- background video -->
+        <HomeMashupBackground/>
+        <!-- content -->
+        <HomeMashupMain/>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -27,18 +27,6 @@
 
 <script>
 export default {
-    name:"IndexPage",
-    mounted() {
-        window.onscroll = () => {
-            const sections = document.querySelectorAll("section");
-            sections.forEach(s => {
-                const top = s.offsetTop;
-                const id = s.getAttribute("id");
-                if (scrollY >= top - window.innerHeight/2) {
-                    this.$store.commit("nav/updateSection", id);
-                }
-            });
-        }
-    }
+    name:"IndexPage"
 }
 </script>
