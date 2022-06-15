209c033dac4d3e63b3c400e88487e485c5b4ea70
sakusaku000 <harry.spncr@gmail.com>
2022-06-15 14:03:36 +0100

    updated landing background video

--- components/home/Landing/Background.vue
+++ components/home/Landing/Background.vue
@@ -1,14 +1,5 @@
 <template>
-    <div class="pointer-events-none">
-        <div class="absolute top-0 left-0 right-0 bottom-0 grad-ov">
-            <video autoplay muted loop playsinline src="~/assets/mashup/mashup_bg_loop_sm.mp4" class="w-full h-full filter brightness-90"></video>
-        </div>
+    <div class="absolute top-0 left-0 right-0 bottom-0 grad-ov pointer-events-none">
+        <video autoplay muted loop playsinline src="~/assets/mashup/mashup_bg_loop_sm.mp4" class="w-full h-full filter brightness-90 object-cover"></video>
     </div>
 </template>
-
-<style scoped>
-    .grad-ov video {
-        object-fit: cover;
-        opacity:0.8;
-    }
-</style>
